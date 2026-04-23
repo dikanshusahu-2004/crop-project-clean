3f958489a88cbca49339a8cc5ccb161d7ef1a284
dikanshu <sdikanshu@gmail.com>
2026-04-23 17:27:09 +0530

    use env mongo uri

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://dikanshusahu_db_user:52D4bAdZxN91xEIL@cluster0.abc123.mongodb.net/cropDB?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✅ MongoDB Connected"))
 .catch(err => console.log("❌ DB Error:", err));
 // ✅ routes
